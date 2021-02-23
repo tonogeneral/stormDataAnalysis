86255de755c076a944480cc9e57f2ae0cd70d7cd
Gabriel General <tonogeneral@gmail.com>
2021-02-22 21:11:10 -0300

    Adding method to get, read and store file from url

--- stormDataAnalysis.R
+++ stormDataAnalysis.R
@@ -1,1 +1,14 @@
-print("Hello World")
+
+url = "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+dataDir = "./data"
+DataFN ="StormData.csv.bz2"
+
+if (!file.exists(dataDir)) {
+  dir.create(dataDir)
+  download.file(url, destfile = "./data/StormData.csv.bz2")
+  stormData <- read.csv("./data/StormData.csv.bz2")
+  #unzip(zipfile = DataFileName, exdir = dataDir)
+}
+
+
+head(stormData)
