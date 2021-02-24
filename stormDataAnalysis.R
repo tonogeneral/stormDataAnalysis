c639a1b0945a9dee5203f37d652d2e0c6c770fa0
Gabriel General <tonogeneral@gmail.com>
2021-02-24 01:52:16 -0300

    Plotting the results of the assignments questions.

--- stormDataAnalysis.R
+++ stormDataAnalysis.R
@@ -1,3 +1,9 @@
+# Librarys needed
+
+
+
+
+# Download, store and read de file
 
 url = "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 dataDir = "./data"
@@ -11,4 +17,51 @@ if (!file.exists(dataDir)) {
 }
 
 
+library(dplyr)
+library(ggplot2)
+
+
 head(stormData)
+
+# sum(is.na(stormData$COUNTYENDN) == FALSE)
+# sum(is.na(stormData))
+
+
+stormData$BGN_DATE<- format(as.POSIXct(stormData$BGN_DATE,format='%m/%d/%Y %H:%M:%S'),format='%m/%d/%Y')
+
+#stormData1 <-stormData[!complete.cases(stormData),]
+# Subsetting dataset with useful columns
+dataDMN <- subset(stormData, FATALITIES + INJURIES >0, select = c(STATE,BGN_DATE,COUNTYNAME,STATE,EVTYPE,FATALITIES,INJURIES,PROPDMG))
+dataDMG <- subset(stormData, PROPDMG >0, select = c(STATE,BGN_DATE,COUNTYNAME,STATE,EVTYPE,FATALITIES,INJURIES,PROPDMG))
+
+#dataDMN$BGN_DATE<- format(as.POSIXct(dataDMN$BGN_DATE,format='%m/%d/%Y %H:%M:%S'),format='%m/%d/%Y')
+
+
+str(dataDMN)
+head(dataDMN)
+tail(dataDMN)
+
+
+
+
+
+danger <- dataDMN %>%
+  group_by(EVTYPE) %>%
+  summarise(DAMN = sum(INJURIES+FATALITIES)) %>%
+  top_n(5)
+
+econ <- dataDMG %>%
+  group_by(EVTYPE) %>%
+  summarise(DMG = sum(PROPDMG)) %>%
+  top_n(5)
+
+
+ggplot(danger, aes(EVTYPE, DAMN)) + 
+  geom_bar(stat="identity") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust = 1))
+
+
+ggplot(econ, aes(EVTYPE, DMG)) + 
+  geom_bar(stat="identity") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust = 1))
+
